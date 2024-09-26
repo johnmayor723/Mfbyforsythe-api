1dc70b972af3a95b5177ffcdb7f2787ff06b9f8b
may <may@xymail.com>
2024-09-26 12:29:47 +0100

    first milestone

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const authRoutes = require("./routes/auth")
+const connectDB = require("./config/database")
+
 const PORT = process.env.PORT || 3000;  // Use the port assigned by Render or default to 3000
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB()
+
+// 
+app.use("/api/auth", authRoutes)
+
 
 // Root route
 app.get('/', (req, res) => {
