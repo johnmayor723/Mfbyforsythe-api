98e9a9a47d83073f97c4264faa45c70475b1e99e
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2024-10-20 20:04:03 +0100

    Update cartRoutes.js

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -11,8 +11,8 @@ const {
 
 // routes/cart.js
 
-const express = require('express');
-const router = express.Router();
+//const express = require('express');
+//const router = express.Router();
 const cartController = require('../controllers/cartController');
 
 router.get('/', cartController.getCart);
