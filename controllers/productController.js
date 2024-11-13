df0f0d19388b4e0188a2b693d8e460a507677e95
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2024-11-13 16:46:20 +0100

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -30,6 +30,29 @@ exports.createProduct = async (req, res) => {
   }
 };
 
+// Get all products
+exports.getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};
+
+// Get product by ID
+exports.getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+};
+
 // Update product
 exports.updateProduct = async (req, res) => {
   try {
